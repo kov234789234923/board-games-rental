1a28e367cb3c1b393f78c13dcdb40f200fe6fad9
alexandrakovalenko <nicoco752@gmail.com>
2026-06-03 01:50:35 +0300

    Commit to dev
    Завершена разработка сервера и развернута база данных
    Развернута БД BoardGamesDB через миграции EF CoreРеализован паттерн Стратегия и покрыт модульными тестами xUnitНастроен Web API сервер и добавлен контроллер для работы с настольными играми по сети

---
+++ BoardGamesRental/BoardGames.Server/Controllers/GamesController.cs
@@ -0,0 +1,23 @@
+﻿using BoardGames.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace BoardGames.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Адрес в сети будет: http://localhost:5000/api/games
+    public class GamesController : ControllerBase
+    {
+        private readonly BoardGamesContext _context = new();
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BoardGame>>> GetGames()
+        {
+            return await _context.BoardGames.Include(g => g.Category).ToListAsync();
+        }
+    }
+}
--- BoardGamesRental/BoardGames.Server/Program.cs
+++ BoardGamesRental/BoardGames.Server/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using BoardGames.Server;
+using BoardGames.Server.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
+
+
+builder.Services.AddDbContext<BoardGamesContext>();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<BoardGamesContext>();
+
+    if (!context.Categories.Any())
+    {
+        var strategyCategory = new Category { Name = "Стратегии" };
+        var partyCategory = new Category { Name = "Пати-геймы" };
+
+        context.Categories.AddRange(strategyCategory, partyCategory);
+
+        context.BoardGames.AddRange(
+            new BoardGame { Title = "Монополия", PriceSale = 2500, PriceRentPerDay = 150, DepositAmount = 1000, Category = strategyCategory },
+            new BoardGame { Title = "Мафия", PriceSale = 900, PriceRentPerDay = 50, DepositAmount = 400, Category = partyCategory },
+            new BoardGame { Title = "Каркассон", PriceSale = 2100, PriceRentPerDay = 120, DepositAmount = 800, Category = strategyCategory }
+        );
+        context.SaveChanges();
+    }
+}
+
+app.MapControllers();
+
+
+app.Run("http://localhost:5000");
